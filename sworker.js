5342f8e1486d005ccb0de8877fe185423e256c0a
ajmalvp <ajmalaju5758559@gmail.com>
2020-08-17 11:26:20 +0400

    sworker adding css file

--- sworker.js
+++ sworker.js
@@ -2,7 +2,7 @@ self.addEventListener("install",e=>{
     
     e.waitUntil(
         caches.open("static").then(caches=>{
-            return caches.addAll(["./","./images/logo 64.png","./images/logo 512.png"]);
+            return caches.addAll(["./","./images/logo 64.png","./images/logo 512.png","style1/style.css","style1/bootstrap.css"]);
         })
     )
 
